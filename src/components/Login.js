3b4289dfd129d16fd7ff8d85fb17f7a308eae2d5
restuu <restu_u@outlook.com>
2018-07-26 15:31:31 +0700

    on progress login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Card, Text, Button } from 'react-native-elements';
+
+import styles from '../styles/styles';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      password: '',
+      email: '',
+      fullname: '',
+    };
+  }
+
   render() {
     return (
-      <View>
-        <Text>
-          Login Page
-        </Text>
+      <View style={styles.container}>
+        <Card containerStyle={styles.headlineContainer}>
+          <Text h3 style={styles.headlineText}>
+            Login Page
+          </Text>
+        </Card>
       </View>
     );
   }
