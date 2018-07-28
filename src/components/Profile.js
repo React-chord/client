831ead4a526aea0c37d906e0b7b474b4b188ca84
restuu <restu_u@outlook.com>
2018-07-28 21:22:05 +0700

    if token is stored, ask user detail to server. home screen route and profile route divide to stack navigation

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
+import styles from '../styles/styles';
+
 class Profile extends Component {
   state = { }
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>
           Profile Page
         </Text>
